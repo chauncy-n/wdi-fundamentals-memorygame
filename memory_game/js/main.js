70eebad34bcc65c9ca93c0bcf0caba3452d00848
chauncy-n <eldoon@comcast.net>
2018-08-15 08:07:37 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+var cardOne = "queen";
+
+var cardTwo = "queen";
+
+var cardThree = "king";
+
+var cardFour = "king";
+
+console.log("User flippeed " + cardFour);
+
+console.log("User flipped " + cardThree);
